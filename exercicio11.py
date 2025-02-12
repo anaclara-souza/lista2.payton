9d2b8dd96813664cf8c1bd8c1618ebac9df66df6
anaclara-souza <1234anasouza5678@gmail.com>
2025-02-12 20:57:35 -0300

    atividade

--- exercicio11.py
+++ exercicio11.py
@@ -1,6 +1,5 @@
 #escreva um programa que pergunte o nome do aniversariante,em seguida pergunte a sua idade acrecente mais 1 e exiba o nome do aniversariante seguda da mensagen "no proximo aniversario voce teraidade + anos"
 nome = input  ("Qual e seu nome aniversariante?:")
-
 idade = int (input("Qual e a sua idade?:"))
 mais = 1 
 print("Seu nome é",nome,"é no proximo ano voce fara ",idade + mais,"anos.") 
